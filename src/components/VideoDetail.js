68f52d217e18650891d62b04064df27fe22c6a3a
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-05 19:38:20 +0200

    video player

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
+  const videoSrc = `https://www.youtube.com/embed/${video?.id.videoId}`;
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header"> {video?.snippet.title}</h4>
         <p>{video?.snippet.description}</p>
