8e2304a3b086d0164c23a3330cffb4b1e36ee63d
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-05 15:27:04 +0200

    rendering video thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const VideoItem = () => {
-  return <div>VideoItem</div>;
+const VideoItem = ({ video }) => {
+  return (
+    <div>
+      <img src={video.snippet.thumbnails.high.url} alt="car" />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 const VideoList = ({ videos }) => {
-  const renderedList = videos.map((video) => <VideoItem />);
+  const renderedList = videos.map((video) => <VideoItem video={video} />);
   console.log(renderedList);
   return <div>{renderedList}</div>;
 };
