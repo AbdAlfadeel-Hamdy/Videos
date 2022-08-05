743365df34734900aa63d31b63c3ee0c25808d69
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-08-05 19:14:11 +0200

    video detail

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
   onTermSubmit = async (term) => {
@@ -11,15 +12,15 @@ class App extends React.Component {
       },
     });
     this.setState({ videos: response.data.items });
-    console.log(response);
   };
   onVideoSelect = (video) => {
-    console.log(video);
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "20px" }}>
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
